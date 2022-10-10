743f0f7ed6969648473b2a4f0cf2d5fbf058b552
YareStu30 <yare.cs22@gmail.com>
2022-10-10 00:31:38 -0300

    limit increase pokemon

--- src/context/ImgProvider.js
+++ src/context/ImgProvider.js
@@ -11,7 +11,7 @@ const ImgProvider = (props) => {
 
   const jsonData = async () => {
     try {
-      const url = "https://pokeapi.co/api/v2/pokemon/?limit=10";
+      const url = "https://pokeapi.co/api/v2/pokemon/?limit=1000";
       const response = await fetch(url);
       const data = await response.json();
       setListName(data);
